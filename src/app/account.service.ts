6ab0561016fa58d71bf1e12b4c29a4a2d151dfe5
Blackbetty312 <rubel8@o2.pl>
2021-02-10 14:38:34 +0100

    added authorization (auth rules from firebase)

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -9,35 +9,38 @@ import { AccountModel } from "../account-model";
 export class AccountService {
   baseUrl =
     "https://angular-project-a6abb-default-rtdb.europe-west1.firebasedatabase.app/";
+  params = { auth: "PQg3mM42raewuqlqiLV4JyyPhNPIrAiJaiLmgqOS" };
   constructor(private http: HttpClient) {}
 
   getAccountList() {
-    return this.http.get(this.baseUrl + "salda.json");
+    return this.http.get(this.baseUrl + "salda.json", { params: this.params });
   }
 
   addNewAccount(model: AccountModel) {
     const body = JSON.stringify(model);
-    return this.http.post(this.baseUrl + "salda.json", body).subscribe(err => {
-      console.log(err);
-    });
+    return this.http
+      .post(this.baseUrl + "salda.json", body, { params: this.params })
+      .subscribe(err => {
+        console.log(err);
+      });
   }
 
   deleteAccountById(id: String) {
     return this.http
-      .delete(this.baseUrl + "salda/" + id + ".json")
+      .delete(this.baseUrl + "salda/" + id + ".json", { params: this.params })
       .subscribe(err => {
         console.log(err);
       });
   }
 
   getAccountById(id: String) {
-    return this.http.get(this.baseUrl + "salda/" + id + ".json");
+    return this.http.get(this.baseUrl + "salda/" + id + ".json", { params: this.params });
   }
 
   updateAccountById(id: String, model: AccountModel) {
     const body = JSON.stringify(model);
     return this.http
-      .put(this.baseUrl + "salda/" + id + ".json", body)
+      .put(this.baseUrl + "salda/" + id + ".json", body, { params: this.params })
       .subscribe(err => {
         console.log(err);
       });
